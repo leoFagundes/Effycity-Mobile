c28b0d245c7c2c15674bce8159a37e694c26a797
leonardof <leonardof@ctir.gov.br>
2024-10-21 12:06:50 -0300

    add google

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 eas login
 
 eas build --platform android
+
+android key -> 375754929781-snp35g2o24lm4uejr0jevo28b3c4fepm.apps.googleusercontent.com
+
+Video Google Tutorial: https://www.youtube.com/watch?v=BDeKTPQzvR4&t=3s
+Minuto: 23:01
--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -8,15 +8,45 @@ import { Link, useRouter } from "expo-router";
 import { useEffect, useState } from "react";
 import ManagerUserRepository from "@/services/repositories/managerUserRepository";
 import { Loading } from "@/components/loading";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export function Home() {
   const [loading, setLoading] = useState(false);
+  const [userInfo, setUserInfo] = useState(null);
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    androidClientId:
+      "375754929781-snp35g2o24lm4uejr0jevo28b3c4fepm.apps.googleusercontent.com",
+  });
 
   const router = useRouter();
 
+  const getUserInfo = async (token: string) => {
+    if (!token) return;
+    try {
+      const response = await fetch("https://www.googleapis/userinfo/v2/me", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      // error
+    }
+  };
+
   async function onGoogleSubmit() {
+    promptAsync();
+
     setLoading(true);
     try {
+      const user = await AsyncStorage.getItem("@user");
+
+      if (!user) {
+      } else {
+        setUserInfo(JSON.parse(user));
+      }
       // const fetchedUsers = await ManagerUserRepository.getAll();
       router.push("/login");
     } catch (error) {
